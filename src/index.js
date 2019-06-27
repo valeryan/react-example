acd9038cfdf70c49001c4edf6c4c3b8736118352
Samuel Hilson <samrhilson@gmail.com>
2019-06-26 20:59:01 -0500

    Adding content from lesson 1.7

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import './scss/app.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+const render = () => {
+  document.getElementById('root').innerHTML = `
+    <div>
+      Hello HTML
+      <input class="form-control" />
+      <pre>${(new Date).toLocaleTimeString()}</pre>
+    </div>
+  `;
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-);
+  ReactDOM.render(
+    React.createElement(
+      "div",
+      null,
+      "Hello React",
+      React.createElement('input', {className: 'form-control'}),
+      React.createElement('pre', null, (new Date).toLocaleTimeString())
+    ),
+    document.getElementById('root2'),
+  );
+}
+
+setInterval(render, 1000);
